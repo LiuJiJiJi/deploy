4488a1a5264fee01b27ee7d795417addbff09283
liuji <1183925272@qq.com>
2021-04-01 21:16:30 +0800

    [1.0] gitlab runner

--- script/install_gitlab_runner.sh
+++ script/install_gitlab_runner.sh
@@ -5,7 +5,7 @@ echo "Install gitlab runner start"
 # curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
 echo "Please upload gitlab-runner_amd64.deb to /script"
 echo "-----> scp -r gitlab-runner_amd64.deb ubuntu@xx.xx.xx.xx:/home/ubuntu/deploy/script"
-dpkg -i gitlab-runner_amd64.deb
+sudo dpkg -i gitlab-runner_amd64.deb
 # input REGISTRATION_TOKEN
 echo -n "Please input REGISTRATION_TOKEN:"
 read REGISTRATION_TOKEN
