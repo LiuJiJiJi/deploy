0e949e38e045edfbe2e6e77be266b553c5faf2e0
liuji <1183925272@qq.com>
2021-04-01 16:16:22 +0800

    [1.0] update uninstall_docker.sh

--- script/uninstall_docker.sh
+++ script/uninstall_docker.sh
@@ -4,11 +4,12 @@ echo "uninstall Start"
 
 # uninstall envsubst openssl
 sudo apt-get remove envsubst openssl
-
 # uninstall docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
 # uninstall docker-compose
 sudo rm -rf /usr/local/bin/docker-compose
+
+sudo sudo apt autoremove
 
 echo "Success"
