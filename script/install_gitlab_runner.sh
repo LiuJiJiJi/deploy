78005d91bfd96e57b480c35411879722d32a6a06
liuji <1183925272@qq.com>
2021-04-01 22:34:12 +0800

    [1.0] python

--- script/install_gitlab_runner.sh
+++ script/install_gitlab_runner.sh
@@ -3,11 +3,11 @@
 echo "Install gitlab runner start"
 
 # curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
-echo "Please upload gitlab-runner_amd64.deb to /script"
-echo "-----> scp -r gitlab-runner_amd64.deb ubuntu@xx.xx.xx.xx:/home/ubuntu/deploy/script"
+echo -e -n "Please upload gitlab-runner_amd64.deb to /script"
+echo -e -n "      -----> scp -r gitlab-runner_amd64.deb ubuntu@xx.xx.xx.xx:/home/ubuntu/deploy/script"
 sudo dpkg -i gitlab-runner_amd64.deb
 # input REGISTRATION_TOKEN
-echo -n "Please input REGISTRATION_TOKEN:"
+echo -e -n "\033[31m Please input REGISTRATION_TOKEN: \033[0m"
 read REGISTRATION_TOKEN
 sudo gitlab-runner register --url https://gitlab.com/ --registration-token $REGISTRATION_TOKEN
 sudo systemctl enable gitlab-runner
