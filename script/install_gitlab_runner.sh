456049ddd2697772b28cc643a419b927c23911df
liuji <1183925272@qq.com>
2021-04-01 21:13:40 +0800

    [1.0] gitlab runner

---
+++ script/install_gitlab_runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Install gitlab runner start"
+
+# curl -LJO "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb"
+echo "Please upload gitlab-runner_amd64.deb to /script"
+echo "-----> scp -r gitlab-runner_amd64.deb ubuntu@xx.xx.xx.xx:/home/ubuntu/deploy/script"
+dpkg -i ./gitlab/gitlab-runner_amd64.deb
+# input REGISTRATION_TOKEN
+echo -n "Please input REGISTRATION_TOKEN:"
+read REGISTRATION_TOKEN
+sudo gitlab-runner register --url https://gitlab.com/ --registration-token $REGISTRATION_TOKEN
+
+echo "Install gitlab runner end"
---
+++ script/uninstall_gitlab_runner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Install gitlab runner start"
+
+sudo apt-get remove --purge gitlab-runner*
+
+echo "Install gitlab runner end"
