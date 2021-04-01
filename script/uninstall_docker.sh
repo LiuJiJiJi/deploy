74de6833370c9e02b78ec5bc95df5ae1c3c9de69
liuji <1183925272@qq.com>
2021-04-01 16:13:28 +0800

    [1.0] update

--- script/uninstall_docker.sh
+++ script/uninstall_docker.sh
@@ -2,6 +2,9 @@
 
 echo "uninstall Start"
 
+# uninstall envsubst openssl
+sudo apt-get remove envsubst openssl
+
 # uninstall docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
