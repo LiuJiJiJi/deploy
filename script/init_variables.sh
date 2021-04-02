1ea47be1d4bda2394ee68bcb06e504aec706ae9c
liuji <1183925272@qq.com>
2021-04-02 22:26:20 +0800

    [1.0] update init_variables.sh

--- script/init_variables.sh
+++ script/init_variables.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 ips=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"​`
-for value in $ips
+
+for value in ${ips}
 do
-    if[[ $value =="10."*]] ;then
-      echo "ip4: $value"
-      export IP4="$value"
-    else
-      echo "$value"
-    fi
+  if [[ "$value" =~ "10" ]]
+  then
+    echo "ip4: $value"
+    export IP4="$value"
+  fi
 done
 
