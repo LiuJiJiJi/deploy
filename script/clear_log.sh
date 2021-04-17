5ddedfd0d90f89243bf8cc7b61bb8895a60ffd12
liuji <1183925272@qq.com>
2021-04-17 21:35:11 +0800

    [log]

--- script/clear_log.sh
+++ script/clear_log.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "==================== start clean docker containers logs =========================="
 
-logs=$(find /var/lib/docker/containers/ -name *-json.log)
+logs=$(sudo find /var/lib/docker/containers/ -name *-json.log)
 
 for log in $logs
     do
