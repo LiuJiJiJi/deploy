f5702a6694e1f0026a3b6e7722c677d42fb3280b
liuji <1183925272@qq.com>
2021-04-17 14:49:01 +0800

    [nvm]

--- script/install_java_node_python.sh
+++ script/install_java_node_python.sh
@@ -34,9 +34,7 @@ sudo chmod a+rw /usr/share/maven
 # sudo yarn config set registry https://registry.npm.taobao.org
 # nvm
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.37.2/install.sh | bash
-# echo 'source $HOME/.nvm/nvm.sh' >> ~/.profile
-echo 'sleep 6s, for waitting ~/.profile edit finish.'
-source ~/.profile
+cat ~/.profile
 source ~/.profile
 nvm --version
 nvm install 10
