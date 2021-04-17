fe543f40d91cb515c6dfcacb98731a3cf8fb7354
liuji <1183925272@qq.com>
2021-04-17 14:37:39 +0800

    [-y]

--- script/uninstall_gitlab_runner.sh
+++ script/uninstall_gitlab_runner.sh
@@ -5,6 +5,6 @@ echo "Install gitlab runner start"
 echo -n "Please input runner name:"
 read runner_name
 sudo gitlab-runner unregister --name $runner_name
-sudo apt-get remove --purge gitlab-runner
+sudo apt-get -y remove --purge gitlab-runner
 
 echo "Install gitlab runner end"
